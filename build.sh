600af14709a60de96d2479ef71d33911cda1ea1d
Gleb Golubitsky <sectoid@gnolltech.org>
2017-08-04 21:50:35 +0300

    [BUILD] Creating crate for the final offline solver
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 cd lambda_punter/
-
 cargo build || exit $?
 cargo test || exit $?
+cd ..
+
+cd lambda_punter_offline/
+cargo build || exit $?
+cargo test || exit $?
+cd ..
 
 exit 0
