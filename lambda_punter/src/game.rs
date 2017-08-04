ec91f22a1bdc70649bbc13592dc2883e6975cca1
Alexey Voznyuk <me@swizard.info>
2017-08-04 17:39:13 +0300

    store moves in state as well

--- lambda_punter/src/game.rs
+++ lambda_punter/src/game.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 use super::types::PunterId;
 use super::map::Map;
@@ -8,6 +9,7 @@ pub struct GameState {
     punter: PunterId,
     punters_count: usize,
     map: Map,
+    moves: HashMap<PunterId, Vec<Move>>,
 }
 
 impl GameState {
@@ -16,10 +18,22 @@ impl GameState {
             punter: setup.punter,
             punters_count: setup.punters,
             map: setup.map,
+            moves: HashMap::new(),
         }
     }
 
-    pub fn play(self, _moves: &[Move]) -> (Move, GameState) {
+    pub fn play(mut self, moves: Vec<Move>) -> (Move, GameState) {
+        for mv in moves {
+            let punter = match mv {
+                Move::Claim { punter, .. } => punter,
+                Move::Pass { punter, } => punter,
+            };
+            self.moves
+                .entry(punter)
+                .or_insert_with(Vec::new)
+                .push(mv);
+        }
+
         (Move::Pass { punter: self.punter, }, self)
     }
 }
