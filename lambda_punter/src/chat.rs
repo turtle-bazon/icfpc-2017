c414f9ead33709effa37e231cd24d45a963aa45b
Alexey Voznyuk <me@swizard.info>
2017-08-04 18:12:06 +0300

    full server online chat

--- lambda_punter/src/chat.rs
+++ lambda_punter/src/chat.rs
@@ -1,27 +1,58 @@
 
-use super::proto::{Req, Rep};
+use super::proto::{Req, Rep, Score};
+use super::game::GameState;
 
 #[derive(PartialEq, Debug)]
 pub enum Error<SR, RR> {
     Send(SR),
     Recv(RR),
     UnexpectedHandshakeRep(Rep),
+    UnexpectedSetupRep(Rep),
+    UnexpectedMoveRep(Rep),
 }
 
-pub fn run_online<FS, SR, FR, RR>(name: &str, mut send_fn: FS, mut recv_fn: FR) -> Result<(), Error<SR, RR>>
+pub fn run_online<FS, SR, FR, RR>(name: &str, mut send_fn: FS, mut recv_fn: FR) -> Result<Vec<Score>, Error<SR, RR>>
     where FS: FnMut(Req) -> Result<(), SR>,
           FR: FnMut() -> Result<Rep, RR>,
 {
+    // P → S {"me" : name}
     send_fn(Req::Handshake { name: name.to_string(), })
         .map_err(Error::Send)?;
+    // S → P {"you" : name}
     match recv_fn().map_err(Error::Recv)? {
         Rep::Handshake { name: ref rep_name, } if rep_name == name =>
             (),
         other =>
             return Err(Error::UnexpectedHandshakeRep(other)),
     }
+    // S → P {"punter" : p, "punters" : n, "map" : map}
+    let mut game_state =
+        match recv_fn().map_err(Error::Recv)? {
+            Rep::Setup(setup) =>
+                GameState::new(setup),
+            other =>
+                return Err(Error::UnexpectedSetupRep(other)),
+        };
+    // P → S {"ready" : p}
+    send_fn(Req::Ready { punter: game_state.punter, })
+        .map_err(Error::Send)?;
+    // gameplay
+    loop {
+        // S → P {"move" : {"moves" : moves}}
+        // S → P {"stop" : {"moves" : moves,"scores" : scores}}
+        match recv_fn().map_err(Error::Recv)? {
+            Rep::Move { moves, } => {
+                let (move_, next_game_state) = game_state.play(moves);
+                game_state = next_game_state;
+                send_fn(Req::Move(move_)).map_err(Error::Send)?;
+            },
+            Rep::Stop { scores, .. } =>
+                return Ok(scores),
+            other =>
+                return Err(Error::UnexpectedMoveRep(other)),
+        }
 
-    Ok(())
+    }
 }
 
 #[cfg(test)]
