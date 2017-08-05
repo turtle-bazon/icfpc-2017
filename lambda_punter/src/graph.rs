03e0144a45af069781d42bf1c3fc5d5966b4482a
Alexey Voznyuk <me@swizard.info>
2017-08-05 12:41:56 +0300

    `GraphCache` impl

--- lambda_punter/src/graph.rs
+++ lambda_punter/src/graph.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, BinaryHeap};
 
 use super::types::SiteId;
@@ -5,6 +6,21 @@ use super::map::Map;
 
 pub struct Graph {
     neighs: HashMap<SiteId, HashSet<SiteId>>,
+}
+
+#[derive(Default)]
+pub struct GraphCache {
+    pqueue: BinaryHeap<PQNode>,
+    visited: HashSet<SiteId>,
+    path: Vec<SiteId>,
+}
+
+impl GraphCache {
+    fn clear(&mut self) {
+        self.pqueue.clear();
+        self.visited.clear();
+        self.path.clear();
+    }
 }
 
 impl Graph {
@@ -27,5 +43,28 @@ impl Graph {
         Graph {
             neighs: neighs,
         }
+    }
+
+    pub fn shortest_path<'a>(&self, source: SiteId, target: SiteId, cache: &'a mut GraphCache) -> &'a [SiteId] {
+        unimplemented!()
+    }
+}
+
+#[derive(PartialEq, Eq)]
+struct PQNode {
+    site: SiteId,
+    cost: usize,
+}
+
+impl Ord for PQNode {
+    fn cmp(&self, other: &PQNode) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.site.cmp(&other.site))
+    }
+}
+
+impl PartialOrd for PQNode {
+    fn partial_cmp(&self, other: &PQNode) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
