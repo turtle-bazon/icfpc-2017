348ef1c5b00eb6702ad81328a89f8b33afdc3e1e
Gleb Golubitsky <sectoid@gnolltech.org>
2017-08-05 12:58:01 +0300

    [CI] Use compression for passing the sources tarball
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -7,23 +7,23 @@ TEMP_DIR=`mktemp -d`
 TEMP_GIT=$TEMP_DIR/git
 
 git clone --mirror $REPO_URL $TEMP_GIT
-git --git-dir=$TEMP_GIT archive --format=tar $BUILD_REF > $TEMP_DIR/build.tar
+git --git-dir=$TEMP_GIT archive --format=tar $BUILD_REF | gzip > $TEMP_DIR/build.tar.gz
 
 cat > $TEMP_DIR/build.sh <<\EOF
 #!/bin/bash
 
 TEMP_DIR=`mktemp -d`
 
-cat << '!!!' | base64 -d > $TEMP_DIR/build.tar
+cat << '!!!' | base64 -d > $TEMP_DIR/build.tar.gz
 EOF
-base64 -w 0 $TEMP_DIR/build.tar >> $TEMP_DIR/build.sh
+base64 -w 0 $TEMP_DIR/build.tar.gz >> $TEMP_DIR/build.sh
 echo >> $TEMP_DIR/build.sh
 echo '!!!' >> $TEMP_DIR/build.sh
 
 cat >> $TEMP_DIR/build.sh <<\EOF
 cd $TEMP_DIR
-tar xf build.tar
-rm -f build.tar
+tar xf build.tar.gz
+rm -f build.tar.gz
 
 ./build.sh 2>&1
 RETVAL=$?
