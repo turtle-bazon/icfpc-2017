b461c01c867fe80b965a9f7df9845bc1b11d10b4
Gleb Golubitsky <sectoid@gnolltech.org>
2017-08-05 12:42:05 +0300

    [CI] Merge all CI build scripts into the single
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -9,11 +9,30 @@ TEMP_GIT=$TEMP_DIR/git
 git clone --mirror $REPO_URL $TEMP_GIT
 git --git-dir=$TEMP_GIT archive --format=tar $BUILD_REF > $TEMP_DIR/build.tar
 
-cat build-dist.sh > $TEMP_DIR/build.sh
+cat > $TEMP_DIR/build.sh <<\EOF
+#!/bin/bash
+
+TEMP_DIR=`mktemp -d`
+
+cat << '!!!' | base64 -d > $TEMP_DIR/build.tar
+EOF
 base64 -w 0 $TEMP_DIR/build.tar >> $TEMP_DIR/build.sh
 echo >> $TEMP_DIR/build.sh
 echo '!!!' >> $TEMP_DIR/build.sh
-cat build-dist-tail.sh >> $TEMP_DIR/build.sh
+
+cat >> $TEMP_DIR/build.sh <<\EOF
+cd $TEMP_DIR
+tar xf build.tar
+rm -f build.tar
+
+./build.sh 2>&1
+RETVAL=$?
+
+cd /
+rm -rf $TEMP_DIR
+
+exit $RETVAL
+EOF
 
 cat $TEMP_DIR/build.sh | ssh punter@localhost -p 2222
 RETVAL=$?
